9e19a9f52d28aa940880c9c82b3d08610a7ede86
Ciera Martinez <ciera.martinez@gmail.com>
2019-04-08 14:15:45 -0700

    making a minimal example

--- binder/install.R
+++ binder/install.R
@@ -1,13 +1,11 @@
 ### install regular packages
 
-install.packages("reticulate") # python support in RMarkdown
 install.packages("ggplot2") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
-
-### install bioconductor packages
-# install.packages("BiocManager")
-# BiocManager::install("package")
+install.packages("tidyverse")
+install.packages("skimr")
+install.packages("taxize")
 
 ### install GitHub packages (tag = commit, branch or release tag)
-# install.packages("devtools")
+install.packages("devtools")
 # devtools::install_github("user/repo", ref = "tag")
